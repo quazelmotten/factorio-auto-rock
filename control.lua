cf23091a5e637beaa8f7cfdd978b750e4f4d1c5c
memelin <fifilikeit@gmail.com>
2023-07-09 16:05:08 +0300

    tree obstacles in the way are mined too

--- control.lua
+++ control.lua
@@ -19,7 +19,7 @@ function find_closest_big_rock()
   return closest_entity
 end
 
-function mine_closest_big_rock()
+function mine_closest_big_rocks()
   local player = game.players[1]
   local entity = find_closest_big_rock()
 
@@ -49,21 +49,77 @@ function mine_closest_big_rock()
       direction_value = defines.direction.northeast
     end
 
-    -- Move the player towards the entity
-    player.walking_state = {
-      walking = true,
-      direction = direction_value
-    }
+    -- Calculate the new position of the player
+    local new_pos = {x = position.x, y = position.y}
+    if direction_value == defines.direction.north then
+      new_pos.y = new_pos.y - 1
+    elseif direction_value == defines.direction.northeast then
+      new_pos.x = new_pos.x + 0.707
+      new_pos.y = new_pos.y - 0.707
+    elseif direction_value == defines.direction.east then
+      new_pos.x = new_pos.x + 1
+    elseif direction_value == defines.direction.southeast then
+      new_pos.x = new_pos.x + 0.707
+      new_pos.y = new_pos.y + 0.707
+    elseif direction_value == defines.direction.south then
+      new_pos.y = new_pos.y + 1
+    elseif direction_value == defines.direction.southwest then
+      new_pos.x = new_pos.x - 0.707
+      new_pos.y = new_pos.y + 0.707
+    elseif direction_value == defines.direction.west then
+      new_pos.x = new_pos.x - 1
+    elseif direction_value == defines.direction.northwest then
+      new_pos.x = new_pos.x - 0.707
+      new_pos.y = new_pos.y - 0.707
+    end
 
-    -- Wait until the player is close enough to the entity
-    local distance = math.sqrt((player.position.x - entity.position.x) ^ 2 + (player.position.y - entity.position.y) ^ 2)
-    if distance < 2.5 then
-      -- Mine the entity
-      player.mine_entity(entity, true)
+    -- Check if the new position will cause a collision with any obstacles
+    local collision_box = player.character.prototype.collision_box
+    local bounding_box = {
+      left_top = {x = new_pos.x + collision_box.left_top.x, y = new_pos.y + collision_box.left_top.y},
+      right_bottom = {x = new_pos.x + collision_box.right_bottom.x, y = new_pos.y + collision_box.right_bottom.y}
+    }
+    local obstacles = player.surface.find_entities_filtered{
+      area = bounding_box,
+      collision_mask = "player-layer",
+      type = {"tree"}
+    }
+    if #obstacles == 0 then
+      -- Move the player towards the entity
+      player.walking_state = {
+        walking = true,
+        direction = direction_value
+      }
+
+      -- Wait until the player is close enough to the entity
+      local distance = math.sqrt((player.position.x - entity.position.x) ^ 2 + (player.position.y - entity.position.y) ^ 2)
+      if distance < 2.5 then
+        -- Mine the entity
+        player.mine_entity(entity, true)
+      end
+    else
+      -- Check if the obstacle is a tree
+      local tree_obstacle = false
+      for _, obstacle in pairs(obstacles) do
+        if obstacle.type == "tree" then
+          tree_obstacle = true
+          if player.can_reach_entity(obstacle) then
+            player.mine_entity(obstacle, true)
+          end
+        end
+      end
+      
+      if not tree_obstacle then
+        -- Stop the player from moving
+        player.walking_state = {
+          walking = false,
+          direction = defines.direction.north
+             }
+            end
     end
   end
 end
 
 script.on_event(defines.events.on_tick, function(event)
-  mine_closest_big_rock()
+  mine_closest_big_rocks()
 end)
